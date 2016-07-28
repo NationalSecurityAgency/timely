e0334448d2897cb57ef494d871cfa76477a020fd
klucar <klucar@gmail.com>
2016-07-28 09:26:36 -0400

    testing websocket from Grafana, getting cookie from document

---
+++ grafana/timely-app/dist/components/websocket.js
@@ -0,0 +1,27 @@
+'use strict';
+
+System.register([], function (_export, _context) {
+  "use strict";
+
+  var WebsocketConfigCtrl;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  return {
+    setters: [],
+    execute: function () {
+      _export('WebsocketConfigCtrl', WebsocketConfigCtrl = function WebsocketConfigCtrl() {
+        _classCallCheck(this, WebsocketConfigCtrl);
+      });
+
+      _export('WebsocketConfigCtrl', WebsocketConfigCtrl);
+
+      WebsocketCtrl.templateUrl = 'components/websocket.html';
+    }
+  };
+});
+//# sourceMappingURL=websocket.js.map
