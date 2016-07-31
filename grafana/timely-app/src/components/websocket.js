1b1458ac6c9a4ef96bd10b8d5b9ac307efc13959
klucar <klucar@gmail.com>
2016-07-31 13:58:42 -0400

    websocket demo skeleton

--- grafana/timely-app/src/components/websocket.js
+++ grafana/timely-app/src/components/websocket.js
@@ -1,5 +1,5 @@
 
-export class WebsocketConfigCtrl {
+export class WebsocketCtrl {
 
   constructor() {
 
--- grafana/timely-app/src/module.js
+++ grafana/timely-app/src/module.js
@@ -1,11 +1,11 @@
 import {TimelyAppConfigCtrl} from './components/config';
 import {MetricsPageCtrl} from './components/metrics';
 import {AuthsPageCtrl} from './components/authspage';
-import {WebsocketPageCtrl} from './components/websocket';
+import {WebsocketCtrl} from './components/websocket';
 
 export {
   TimelyAppConfigCtrl as ConfigCtrl,
   MetricsPageCtrl,
   AuthsPageCtrl,
-  WebsocketPageCtrl
+  WebsocketCtrl as WebsocketPageCtrl
 };
