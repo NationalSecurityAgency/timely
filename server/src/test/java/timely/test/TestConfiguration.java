4ce4482764e3e647d9b45c92b5e34a4d218b59a6
Jim Klucar <klucar@gmail.com>
2016-08-25 14:29:54 -0400

    changed whitespace for clarity

--- server/src/test/java/timely/test/TestConfiguration.java
+++ server/src/test/java/timely/test/TestConfiguration.java
@@ -7,12 +7,22 @@ public class TestConfiguration {
     public static final String TIMELY_HTTP_ADDRESS_DEFAULT = "localhost";
 
     public static Configuration createMinimalConfigurationForTest() {
-        Configuration cfg = new Configuration().getServer().setIp("127.0.0.1").getServer().setTcpPort(54321).getHttp()
-                .setIp("127.0.0.1").getHttp().setPort(54322).getWebsocket().setIp("127.0.0.1").getWebsocket()
-                .setPort(54323).getAccumulo().setZookeepers("localhost:2181").getAccumulo().setInstanceName("test")
-                .getAccumulo().setUsername("root").getAccumulo().setPassword("secret").getHttp().setHost("localhost")
-                .getSecurity().getSsl().setUseGeneratedKeypair(true).getAccumulo().getWrite().setLatency("2s")
-                .getWebsocket().setTimeout(20);
+        Configuration cfg = new Configuration()
+                .getServer().setIp("127.0.0.1")
+                .getServer().setTcpPort(54321)
+                .getHttp().setIp("127.0.0.1")
+                .getHttp().setPort(54322)
+                .getHttp().setHost("localhost")
+                .getWebsocket().setIp("127.0.0.1")
+                .getWebsocket().setPort(54323)
+                .getWebsocket().setTimeout(20)
+                .getAccumulo().setZookeepers("localhost:2181")
+                .getAccumulo().setInstanceName("test")
+                .getAccumulo().setUsername("root")
+                .getAccumulo().setPassword("secret")
+                .getAccumulo().getWrite().setLatency("2s")
+                .getSecurity().getSsl().setUseGeneratedKeypair(true);
+
         return cfg;
     }
 
