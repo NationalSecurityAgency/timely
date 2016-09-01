dd30d54ed90d07f6565979719f5e674731e73e9b
Dave Marion <dlmarion@apache.org>
2016-09-01 15:47:32 -0400

    closes #77 - return error when there are too many downsample buckets

--- server/src/main/java/timely/store/DataStoreImpl.java
+++ server/src/main/java/timely/store/DataStoreImpl.java
@@ -457,6 +457,10 @@ public class DataStoreImpl implements DataStore {
                     Map<String, String> orderedTags = orderTags(tagOrder, query.getTags());
                     setQueryColumns(scanner, metric, orderedTags);
                     long downsample = getDownsamplePeriod(query);
+                    if (((endTs - startTs) / downsample + 1) > Integer.MAX_VALUE) {
+                        throw new IOException(
+                                "Downsample not large enough for time range. Decrease time range or increase downsample period.");
+                    }
                     LOG.trace("Downsample period {}", downsample);
                     Class<? extends Aggregator> aggClass = getAggregator(query);
                     LOG.trace("Aggregator type {}", aggClass.getSimpleName());
