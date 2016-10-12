24df5f42733ad4761f295ebea324cecff30ba7ac
Dave Marion <dlmarion@apache.org>
2016-10-12 09:15:38 -0400

    re #91: removed commented code

--- server/src/main/java/timely/sample/Downsample.java
+++ server/src/main/java/timely/sample/Downsample.java
@@ -11,8 +11,6 @@ import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import timely.api.request.timeseries.QueryRequest.RateOption;
 
@@ -49,16 +47,12 @@ public class Downsample implements Iterable<Sample>, Serializable {
 
     }
 
-    private static Logger LOG = LoggerFactory.getLogger(Downsample.class);
-
     private static final long serialVersionUID = 1L;
 
     private transient Aggregator aggregator;
     private final String aggregatorClassname;
     private final long start;
     private final long period;
-    // private final double[] values;
-    // private final int[] counts;
 
     private final TreeMap<Long, AggregatedValue> buckets = new TreeMap<>();
 
@@ -70,12 +64,6 @@ public class Downsample implements Iterable<Sample>, Serializable {
         this.period = period;
         this.aggregator = agg;
         this.aggregatorClassname = agg.getClass().getName();
-        long buckets = (end - start) / period + 1;
-        if (buckets > Integer.MAX_VALUE) {
-            throw new RuntimeException("Cannot downsample to " + buckets + " items");
-        }
-        // values = new double[(int) buckets];
-        // counts = new int[(int) buckets];
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
@@ -150,11 +138,6 @@ public class Downsample implements Iterable<Sample>, Serializable {
             throw new IllegalArgumentException("Empty samples to combine");
         }
         for (Downsample ds : samples) {
-            // if (rateOption != null && rateOption.isCounter()) {
-            // ds = convertCountToRate(ds, rateOption.getResetValue(),
-            // rateOption.getCounterMax());
-            // }
-            // ds = interpolateMissingValues(ds);
             sampleList.add(ds);
         }
         Iterator<Downsample> iter = sampleList.iterator();
@@ -165,68 +148,4 @@ public class Downsample implements Iterable<Sample>, Serializable {
         return result;
     }
 
-    // private static Downsample interpolateMissingValues(Downsample ds) {
-    // Downsample result = new Downsample(ds.start, ds.start + ds.counts.length
-    // * ds.period, ds.period, ds.aggregator);
-    // int lastPos = -1;
-    // for (int i = 0; i < ds.counts.length; i++) {
-    // if (ds.counts[i] != 0) {
-    // result.values[i] = ds.values[i];
-    // result.counts[i] = ds.counts[i];
-    // if (lastPos >= 0) {
-    // double diff = ds.values[i] - ds.values[lastPos];
-    // for (int j = lastPos + 1; j < i; j++) {
-    // result.values[j] = result.values[j - 1] + diff / (i - lastPos);
-    // result.counts[j] = 1;
-    // }
-    // }
-    // lastPos = i;
-    // }
-    // }
-    // LOG.debug("interpolate: {}", result);
-    // return result;
-    // }
-
-    // private static Downsample convertCountToRate(Downsample ds, long
-    // resetValue, long counterMax) {
-    // Downsample result = new Downsample(ds.start + ds.period, ds.start +
-    // ds.counts.length * ds.period, ds.period,
-    // ds.aggregator);
-    // int lastPos = 0;
-    // for (int i = 1; i < ds.counts.length; i++) {
-    // if (ds.counts[i] != 0) {
-    // if (((Long) counterMax).equals(((Double) ds.values[i]).longValue())) {
-    // result.values[i - 1] = resetValue;
-    // result.counts[i - 1] = 1;
-    // } else {
-    // double diff = ds.values[i] - ds.values[lastPos];
-    // if (diff < 0) {
-    // // wrap around formula from
-    // // https://collectd.org/wiki/index.php/Data_source says
-    // // to do
-    // // the following:
-    // // double wrapValue = (counterMax - ds.values[lastPos] +
-    // // ds.values[i]) / (ds.period);
-    // // When counterMax is not defined it defaults to
-    // // Long.MAX_VALUE which leaves wrapValue
-    // // very close to counterMax. This renders the resulting
-    // // graph useless as it blows up the
-    // // scale, we are going to set the value to -1. If we
-    // // have a constantly decreasing counter,
-    // // which in theory should not happen, then all of the
-    // // values will be -1.
-    // double wrapValue = -1.0;
-    // result.values[i - 1] = wrapValue;
-    // result.counts[i - 1] = 1;
-    // } else {
-    // result.values[i - 1] = diff;
-    // result.counts[i - 1] = 1;
-    // }
-    // }
-    // lastPos = i;
-    // }
-    // }
-    // LOG.debug("convertCountToRate: {}", result);
-    // return result;
-    // }
 }
