fdcca9638c0f97d07a9193db024c775c922686cd
Jonathan Stein <jsstein3@illinois.edu>
2020-10-02 00:20:24 -0500

    Fix MetricAdapterTest and QueryResponseTest flakiness

--- server/src/test/java/timely/api/response/timeseries/QueryResponseTest.java
+++ server/src/test/java/timely/api/response/timeseries/QueryResponseTest.java
@@ -24,7 +24,24 @@ public class QueryResponseTest {
         r.putDps("1234567900", 3.5);
         r.putDps("1234567910", 2.5);
         String result = JsonUtil.getObjectMapper().writeValueAsString(Collections.singletonList(r));
-        String expected = "[{\"metric\":\"sys.cpu.user\",\"tags\":{\"rack\":\"r1\",\"host\":\"localhost\"},\"aggregatedTags\":[],\"dps\":{\"1234567890\":4.5,\"1234567900\":3.5,\"1234567910\":2.5}}]";
-        Assert.assertEquals(expected, result);
+        System.out.println("result: " + result);
+        String expectedMetric = "\"metric\":\"sys.cpu.user\"";
+        Assert.assertTrue(result.contains(expectedMetric));
+        String expectedTagsV1 = "\"tags\":{\"rack\":\"r1\",\"host\":\"localhost\"}";
+        String expectedTagsV2 = "\"tags\":{\"host\":\"localhost\",\"rack\":\"r1\"}";
+        Assert.assertTrue(result.contains(expectedTagsV1) || result.contains(expectedTagsV2));
+        String expectedAggregatedTags = "\"aggregatedTags\":[]";
+        Assert.assertTrue(result.contains(expectedAggregatedTags));
+        String expectedDpsV1 = "\"dps\":{\"1234567890\":4.5,\"1234567900\":3.5,\"1234567910\":2.5}";
+        String expectedDpsV2 = "\"dps\":{\"1234567890\":4.5,\"1234567910\":2.5,\"1234567900\":3.5}";
+        String expectedDpsV3 = "\"dps\":{\"1234567900\":3.5,\"1234567890\":4.5,\"1234567910\":2.5}";
+        String expectedDpsV4 = "\"dps\":{\"1234567900\":3.5,\"1234567910\":2.5,\"1234567890\":4.5}";
+        String expectedDpsV5 = "\"dps\":{\"1234567910\":2.5,\"1234567890\":4.5,\"1234567900\":3.5}";
+        String expectedDpsV6 = "\"dps\":{\"1234567910\":2.5,\"1234567900\":3.5,\"1234567890\":4.5,}";
+        Assert.assertTrue(result.contains(expectedDpsV1) || result.contains(expectedDpsV2)
+                || result.contains(expectedDpsV3) || result.contains(expectedDpsV4) || result.contains(expectedDpsV5)
+                || result.contains(expectedDpsV6));
+        Assert.assertEquals("[{", result.substring(0, 2));
+        Assert.assertEquals("}]", result.substring(result.length() - 2));
     }
 }
