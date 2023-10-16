9ad1b18b748d8e07834b66fa4bda16f58cc6dc3c
Bill Oley <bill.oley@gmail.com>
2023-10-16 16:48:55 -0400

    Delete grafana-auth module

--- grafana-auth/src/main/java/timely/grafana/auth/configuration/SpringBootstrap.java
+++
@@ -1,7 +0,0 @@
-package timely.grafana.auth.configuration;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackageClasses = { SpringBootstrap.class })
-public class SpringBootstrap {
-}
