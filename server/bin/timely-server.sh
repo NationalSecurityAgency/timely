ff7a75c7c48625f41ec465a52901c8925eda2fe4
Bill Oley <bill.oley@gmail.com>
2024-04-10 10:48:16 -0400

    Fix extraction of native libraries in scripts, make indentation consistent

--- server/bin/timely-server.sh
+++ server/bin/timely-server.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ $(uname) == "Darwin" ]]; then
-        THIS_SCRIPT=$(python -c 'import os,sys; print os.path.realpath(sys.argv[1])' "$0")
+    THIS_SCRIPT=$(python -c 'import os,sys; print os.path.realpath(sys.argv[1])' "$0")
 else
-        THIS_SCRIPT=$(readlink -f "$0")
-        HW=`uname -m`
-        OS_PATH=linux-${HW}
+    THIS_SCRIPT=$(readlink -f "$0")
+    HW=`uname -m`
+    OS_PATH=linux-${HW}
 fi
 
 if [ -z "$1" ]; then
-  export INSTANCE="1"
+    export INSTANCE="1"
 else
-  export INSTANCE="$1"
+    export INSTANCE="$1"
 fi
 
 PID=$(jps -m | grep -E 'timely-server-.*-exec.jar timelyserver' | grep "instance=${INSTANCE}" | awk '{print $1}')
@@ -19,7 +19,6 @@ PID=$(jps -m | grep -E 'timely-server-.*-exec.jar timelyserver' | grep "instance
 if [ "$PID" == "" ]; then
     THIS_DIR="${THIS_SCRIPT%/*}"
     BASE_DIR=$(cd "$THIS_DIR"/.. && pwd)
-    TMP_DIR="${BASE_DIR}/tmp"
     LIB_DIR="${BASE_DIR}/lib"
     BIN_DIR="${BASE_DIR}/bin"
     export CONF_DIR="${BASE_DIR}/conf"
@@ -32,18 +31,12 @@ if [ "$PID" == "" ]; then
     # use either a value from timely-server-env.sh or the default
     export LOG_DIR="${TIMELY_LOG_DIR:-${BASE_DIR}/logs}"
 
-    if [[ -e ${TMP_DIR} ]]; then
-      rm -rf "${TMP_DIR}"
+    if [[ ! -e ${NATIVE_DIR} ]]; then
+        mkdir -p "${NATIVE_DIR}"
     fi
-    mkdir "${TMP_DIR}"
-
-    if [[ -e ${NATIVE_DIR} ]]; then
-      rm -rf "${NATIVE_DIR}"
-    fi
-    mkdir -p "${NATIVE_DIR}"
 
     if [[ ! -e ${LOG_DIR} ]]; then
-      mkdir "${LOG_DIR}"
+        mkdir "${LOG_DIR}"
     fi
 
     JVM_ARGS="-Xmx4G -Xms4G -XX:NewSize=1G -XX:MaxNewSize=1G"
@@ -54,11 +47,13 @@ if [ "$PID" == "" ]; then
     JVM_ARGS="${JVM_ARGS} -XX:+UseNUMA"
 
     # for linux varieties of OS, extract native libraries if not already present
-    if [[ -n "$OS_PATH" && (! -d ${NATIVE_DIR} || -z "$(ls -A ${NATIVE_DIR})") ]]; then
-        pushd "${BASE_DIR}/bin" || exit
-        "${JAVA_HOME}"/bin/jar xf "${LIB_DIR}"/netty-tcnative-boringssl-static-*.Final-"${OS_PATH}".jar ${NATIVE_DIR}/libnetty_tcnative_linux_"${HW}".so
-        "${JAVA_HOME}"/bin/jar xf "${LIB_DIR}"/netty-transport-native-epoll-*-"${OS_PATH}".jar ${NATIVE_DIR}/libnetty_transport_native_epoll_"${HW}".so
-        popd || exit
+    if [[ -n "$OS_PATH" ]]; then
+        if [[ ! -d ${NATIVE_DIR} || -z "$(ls -A ${NATIVE_DIR})" ]]; then
+            pushd "${BASE_DIR}/bin" || exit
+            "${JAVA_HOME}"/bin/jar xf "${LIB_DIR}"/netty-tcnative-boringssl-static-*.Final-"${OS_PATH}".jar META-INF/native/libnetty_tcnative_linux_"${HW}".so
+            "${JAVA_HOME}"/bin/jar xf "${LIB_DIR}"/netty-transport-native-epoll-*-"${OS_PATH}".jar META-INF/native/libnetty_transport_native_epoll_"${HW}".so
+            popd || exit
+        fi
         JVM_ARGS="${JVM_ARGS} -Djava.library.path=${NATIVE_DIR}"
     fi
 
--- server/bin/util/get-metrics.sh
+++ server/bin/util/get-metrics.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
 
-if [[ `uname` == "Darwin" ]]; then
-    THIS_SCRIPT=`python -c 'import os,sys; print os.path.realpath(sys.argv[1])' $0`
+if [[ $(uname) == "Darwin" ]]; then
+    THIS_SCRIPT=$(python -c 'import os,sys; print os.path.realpath(sys.argv[1])' "$0")
 else
-    THIS_SCRIPT=`readlink -f $0`
+    THIS_SCRIPT=$(readlink -f "$0")
     HW=`uname -m`
     OS_PATH=linux-${HW}
 fi
 
-export INSTANCE="1"
-
 THIS_DIR="${THIS_SCRIPT%/*}"
-BASE_DIR=$(cd $THIS_DIR/../.. && pwd)
+BASE_DIR=$(cd "$THIS_DIR"/.. && pwd)
 LIB_DIR="${BASE_DIR}/lib"
 BIN_DIR="${BASE_DIR}/bin"
 export CONF_DIR="${BASE_DIR}/conf"
-export LOG_DIR="${BASE_DIR}/logs"
+NATIVE_DIR="${BIN_DIR}/META-INF/native"
 
 set -a
-. ${BIN_DIR}/timely-server-env.sh
+. "${BIN_DIR}/timely-server-env.sh"
 set +a
+
+# use either a value from timely-server-env.sh or the default
+export LOG_DIR="${TIMELY_LOG_DIR:-${BASE_DIR}/logs}"
+
+if [[ ! -e ${NATIVE_DIR} ]]; then
+    mkdir -p "${NATIVE_DIR}"
+fi
+
+if [[ ! -e ${LOG_DIR} ]]; then
+    mkdir "${LOG_DIR}"
+fi
 
 JVM_ARGS="-Xmx256m -Xms128m"
 JVM_ARGS="${JVM_ARGS} -Dlogging.config=${CONF_DIR}/log4j2-console.yml"
 JVM_ARGS="${JVM_ARGS} -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
+JVM_ARGS="${JVM_ARGS} -XX:+UseG1GC -XX:+UseStringDeduplication"
+JVM_ARGS="${JVM_ARGS} -Djava.net.preferIPv4Stack=true"
+JVM_ARGS="${JVM_ARGS} -XX:+UseNUMA"
 
 # for linux varieties of OS, extract native libraries if not already present
-if [[ -n "$OS_PATH" && (! -d ${NATIVE_DIR} || -z "$(ls -A ${NATIVE_DIR})") ]]; then
-    pushd "${BASE_DIR}/bin" || exit
-    "${JAVA_HOME}"/bin/jar xf "${LIB_DIR}"/netty-tcnative-boringssl-static-*.Final-"${OS_PATH}".jar ${NATIVE_DIR}/libnetty_tcnative_linux_"${HW}".so
-    "${JAVA_HOME}"/bin/jar xf "${LIB_DIR}"/netty-transport-native-epoll-*-"${OS_PATH}".jar ${NATIVE_DIR}/libnetty_transport_native_epoll_"${HW}".so
-    popd || exit
+if [[ -n "$OS_PATH" ]]; then
+    if [[ ! -d ${NATIVE_DIR} || -z "$(ls -A ${NATIVE_DIR})" ]]; then
+        pushd "${BASE_DIR}/bin" || exit
+        "${JAVA_HOME}"/bin/jar xf "${LIB_DIR}"/netty-tcnative-boringssl-static-*.Final-"${OS_PATH}".jar META-INF/native/libnetty_tcnative_linux_"${HW}".so
+        "${JAVA_HOME}"/bin/jar xf "${LIB_DIR}"/netty-transport-native-epoll-*-"${OS_PATH}".jar META-INF/native/libnetty_transport_native_epoll_"${HW}".so
+        popd || exit
+    fi
     JVM_ARGS="${JVM_ARGS} -Djava.library.path=${NATIVE_DIR}"
 fi
 
