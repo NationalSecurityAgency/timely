e772728159d1d6f8e5cee5a775fcf849e1bdef16
Bill Oley <bill.oley@gmail.com>
2024-04-10 10:50:37 -0400

    Correctly handle load, dwquery, hadoop, accumulo2, metrics in CollectDPluginParent

--- collectd-plugin-parent/src/main/java/timely/collectd/plugin/CollectDPluginParent.java
+++ collectd-plugin-parent/src/main/java/timely/collectd/plugin/CollectDPluginParent.java
@@ -3,6 +3,7 @@ package timely.collectd.plugin;
 import java.io.OutputStream;
 import java.text.MessageFormat;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
@@ -25,6 +26,9 @@ public abstract class CollectDPluginParent {
     private static final String GAUGE = "GAUGE";
     private static final String DERIVE = "DERIVE";
     private static final String ABSOLUTE = "ABSOLUTE";
+    private static final Collection<String> HADOOP_CONTEXTS = Arrays.asList("NameNode", "DataNode", "JobManager", "NodeManager", "JobHistoryServer",
+                    "ResourceManager", "MRAppMaster");
+    private static final Pattern HADOOP_PATTERN_4_GROUPS = Pattern.compile("([\\w-_]+)\\.([\\w-_]+)\\.([\\w-_]+)\\.([\\w-_]+)");
     private static final Pattern STATSD_PATTERN_3_GROUPS = Pattern.compile("([\\w-_]+)\\.([\\w-_]+)\\.([\\w-_]+)");
     private static final Pattern STATSD_PATTERN_4_GROUPS = Pattern.compile("([\\w-_]+)\\.([\\w-_]+)\\.([\\w-_]+)\\.([\\w-_]+)");
     private static final Pattern STATSD_PATTERN_6_GROUPS = Pattern.compile("([\\w-_]+)\\.([\\w-_]+)\\.([\\w-_]+)\\.([\\w-_]+)\\.([\\w-_#]+)\\.([\\w-_]+)");
@@ -64,6 +68,8 @@ public abstract class CollectDPluginParent {
 
     protected void addTag(Map<String,String> m, String k, String v) {
         if (k != null && !k.isBlank() && v != null && !v.isBlank()) {
+            k = k.replaceAll("=", ":");
+            v = v.replaceAll("=", ":");
             m.put(k.trim(), v.trim());
         }
     }
@@ -71,8 +77,8 @@ public abstract class CollectDPluginParent {
     protected void addTag(Map<String,String> m, String kv) {
         if (kv != null && !kv.isBlank()) {
             String[] parts = kv.split("=");
-            if (parts.length == 2 && !parts[0].isBlank() && !parts[1].isBlank()) {
-                m.put(parts[0].trim(), parts[1].trim());
+            if (parts.length == 2 ) {
+                addTag(m, parts[0], parts[1]);
             }
         }
     }
@@ -99,9 +105,10 @@ public abstract class CollectDPluginParent {
         } else {
             addTag(tagMap, "host", host);
         }
-        int nIdx = host.lastIndexOf('n');
+        String[] hostSplit = host.split("\\.");
+        int nIdx = hostSplit[0].lastIndexOf('n');
         if (-1 != nIdx) {
-            addTag(tagMap, "rack", host.substring(0, nIdx));
+            addTag(tagMap, "rack", hostSplit[0].substring(0, nIdx));
         }
         tagMap.putAll(addlTags);
 
@@ -112,22 +119,52 @@ public abstract class CollectDPluginParent {
 
         switch (plugin) {
             case STATSD_PREFIX:
+                Matcher hadoop_4_groups = HADOOP_PATTERN_4_GROUPS.matcher(typeInstance);
                 Matcher statsd_3_groups = STATSD_PATTERN_3_GROUPS.matcher(typeInstance);
                 Matcher statsd_4_groups = STATSD_PATTERN_4_GROUPS.matcher(typeInstance);
                 Matcher statsd_6_groups = STATSD_PATTERN_6_GROUPS.matcher(typeInstance);
                 String instance = null;
                 if (!typeInstance.startsWith("nsq")) {
                     String[] parts = typeInstance.split("\\.");
-                    if (parts.length % 2 == 1) {
-                        if (parts[0].equals("dwquery") && parts.length >= 4) {
-                            metric.append(STATSD_PREFIX).append(PERIOD).append(parts[1]).append(PERIOD).append(parts[3]);
-                            addTag(tagMap, "queryId", parts[0]);
-                        } else {
-                            // EtsyStatsD format -- metric.(tagName.tagValue)*
-                            metric.append(STATSD_PREFIX).append(PERIOD).append(parts[0]);
-                            for (int x = 1; x < parts.length; x += 2) {
-                                addTag(tagMap, parts[x], parts[x + 1]);
+                    if (parts.length >= 4 && (HADOOP_CONTEXTS.contains(parts[0]) || hadoop_4_groups.matches())) {
+                        // Here we are processing the statsd metrics coming from the Hadoop Metrics2 StatsDSink without the host name.
+                        // The format of metric is: serviceName.contextName.recordName.metricName
+                        // The serviceName is used as the instance.
+                        instance = parts[0];
+                        String contextName = parts[1];
+                        int firstPeriod = typeInstance.indexOf(".");
+                        int lastPeriod = typeInstance.lastIndexOf(".");
+                        String metricName = typeInstance.substring(lastPeriod + 1);
+                        String recordName = typeInstance.substring(firstPeriod + 1, lastPeriod);
+                        // the recordName may contain useful tags such as op=<operation> and user<user>
+                        // in addition to a possibly duplicative recordName
+                        String[] recordNameSplits = recordName.split("\\.");
+                        metric.append(STATSD_PREFIX).append(PERIOD).append(contextName).append(PERIOD);
+                        for (String split : recordNameSplits) {
+                            // avoid duplication with either the contextName or instance
+                            if (!split.equalsIgnoreCase(contextName) && !split.equalsIgnoreCase(instance)) {
+                                if (split.startsWith("NNTopUserOpCounts") && split.contains("windowMs=")) {
+                                    metric.append("NNTopUserOpCounts").append(PERIOD);
+                                    addTag(tagMap, split.substring(split.indexOf("windowMs=")));
+                                } else if (split.contains("=")) {
+                                    addTag(tagMap, split);
+                                } else {
+                                    addTag(tagMap, "record", split);
+                                }
                             }
+                        }
+                        metric.append(metricName);
+                    } else if (parts.length >= 2 && parts[1].equals("dwquery")) {
+                        // These come from Datawave's QueryIterator metrics - the first split part is the queryId
+                        // Use statsd plus the rest of the typeInstance after the first period as the metric name
+                        int x = typeInstance.indexOf(".");
+                        metric.append(STATSD_PREFIX).append(PERIOD).append(typeInstance.substring(x + 1));
+                        addTag(tagMap, "queryId", parts[0]);
+                    } else if (parts.length % 2 == 1) {
+                        // EtsyStatsD format -- metric.(tagName.tagValue)*
+                        metric.append(STATSD_PREFIX).append(PERIOD).append(parts[0]);
+                        for (int x = 1; x < parts.length; x += 2) {
+                            addTag(tagMap, parts[x], parts[x + 1]);
                         }
                     } else if (statsd_4_groups.matches()) {
                         // Here we are processing the statsd metrics coming from the Hadoop Metrics2 StatsDSink without the host name.
@@ -218,6 +255,12 @@ public abstract class CollectDPluginParent {
                 metric.append("sys.").append(plugin).append(PERIOD).append(type);
                 addTag(tagMap, INSTANCE, typeInstance.replaceAll(" ", "_"));
                 break;
+            case "load":
+                metric.append("sys.load.").append(plugin).append(PERIOD).append(type);
+                // plugin returns 1 min, 5 min, 15 min averages, but Timely can
+                // downsample on its own so we only use the first (1 min) value
+                vl.setValues(Arrays.asList(vl.getValues().get(0)));
+                break;
             case "GenericJMX":
                 metric.append("sys.").append(plugin).append(PERIOD).append(type).append(PERIOD).append(typeInstance);
                 String[] pluginInstanceSplit = pluginInstance.split("-");
@@ -227,6 +270,7 @@ public abstract class CollectDPluginParent {
                 if (pluginInstanceSplit.length > 1) {
                     addTag(tagMap, NAME, pluginInstanceSplit[1].replaceAll(" ", "_"));
                 }
+                break;
             default:
                 if (notEmpty(type) && notEmpty(typeInstance) && notEmpty(plugin) && notEmpty(pluginInstance)) {
                     metric.append("sys.").append(plugin).append(PERIOD).append(type).append(PERIOD).append(typeInstance);
@@ -253,9 +297,15 @@ public abstract class CollectDPluginParent {
             }
             Double value = vl.getValues().get(i).doubleValue();
             String tagString = tagMap.entrySet().stream().map(e -> e.getKey() + "=" + e.getValue()).collect(Collectors.joining(" "));
-            String datapoint = MessageFormat.format(PUT, metricName, timestamp.toString(), value.toString(), tagString);
-            logDebug(String.format("Writing: %s", datapoint));
-            write(datapoint + "\n", out);
+            
+            if (metricName.isBlank() || timestamp == null || value == null) {
+                logDebug(String.format("Not writing unhandled metric: plugin:%s pluginInstance:%s type:%s typeInstance:%s",
+                                plugin, pluginInstance, type, typeInstance));
+            } else {
+                String datapoint = MessageFormat.format(PUT, metricName, timestamp.toString(), value.toString(), tagString);
+                logDebug(String.format("Writing: %s", datapoint));
+                write(datapoint + "\n", out);
+            }
         }
     }
 
@@ -306,5 +356,4 @@ public abstract class CollectDPluginParent {
     private boolean notEmpty(String arg) {
         return (null != arg) && !(arg.equals(""));
     }
-
 }
