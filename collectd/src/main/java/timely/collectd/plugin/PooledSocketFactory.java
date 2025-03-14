db7aa7be0ad0b648b147808b4a5b0b1e3657924d
Bill Oley <bill.oley@gmail.com>
2025-03-14 09:32:15 -0400

    Refactor collectd plugins into one module

---
+++ collectd/src/main/java/timely/collectd/plugin/PooledSocketFactory.java
@@ -0,0 +1,122 @@
+package timely.collectd.plugin;
+
+import java.net.Socket;
+
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.PooledObjectFactory;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.collectd.api.Collectd;
+import org.collectd.api.OConfigItem;
+
+public class PooledSocketFactory implements PooledObjectFactory<Socket> {
+
+    private String host = null;
+    private int port = 0;
+    private static final long initialBackoff = 2000;
+    private int socketTimeout = 60000; // 60 seconds in milliseconds
+    private long connectionTimeToLive = 300000; // 300 seconds in milliseconds
+
+    public int config(OConfigItem config) {
+        for (OConfigItem child : config.getChildren()) {
+            switch (child.getKey()) {
+                case "host":
+                case "hostname":
+                case "Host":
+                case "HostName":
+                    host = child.getValues().get(0).getString();
+                    break;
+                case "Port":
+                case "port":
+                    port = Integer.parseInt(child.getValues().get(0).getString());
+                    break;
+                case "socketTimeout":
+                case "SocketTimeout":
+                    socketTimeout = Integer.parseInt(child.getValues().get(0).getString());
+                    break;
+                case "connectionTimeToLive":
+                case "ConnectionTimeToLive":
+                    connectionTimeToLive = Long.parseLong(child.getValues().get(0).getString());
+                    break;
+                default:
+
+            }
+        }
+        if (null != host) {
+            return 0;
+        }
+        if (host == null) {
+            Collectd.logError("Timely host must be configured");
+        }
+        if (port == 0) {
+            Collectd.logError("Timely port must be configured");
+        }
+        return -1;
+    }
+
+    private Socket connect() {
+
+        Socket socket = null;
+        long currentBackoff = initialBackoff;
+        while (socket == null) {
+            try {
+                socket = new Socket(host, port);
+                if (socketTimeout > -1) {
+                    socket.setSoTimeout(socketTimeout);
+                }
+                Collectd.logInfo("Connected to Timely at " + host + ":" + port + " from local port:" + socket.getLocalPort());
+            } catch (Exception e) {
+                Collectd.logError("Error connecting to Timely at " + host + ":" + port + ". Error: " + e.getMessage() + ".  Will retry connection in "
+                                + currentBackoff + " ms.");
+                try {
+                    Thread.sleep(currentBackoff);
+                } catch (InterruptedException e1) {
+
+                }
+                // max out reconnect period at one minute
+                currentBackoff = (currentBackoff * 2 > 60000) ? 60000 : currentBackoff * 2;
+            }
+        }
+        return socket;
+    }
+
+    @Override
+    public PooledObject<Socket> makeObject() {
+        return new DefaultPooledObject<>(connect());
+    }
+
+    @Override
+    public void destroyObject(PooledObject<Socket> pooledObject) {
+        try {
+            Collectd.logDebug("Shutting down connection to Timely at " + host + ":" + port);
+            Socket socket = pooledObject.getObject();
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (Exception e) {
+            Collectd.logError("Error closing connection to Timely at " + host + ":" + port + ". Error: " + e.getMessage());
+        } finally {
+            pooledObject.invalidate();
+        }
+    }
+
+    @Override
+    public boolean validateObject(PooledObject<Socket> pooledObject) {
+        if (connectionTimeToLive > -1) {
+            if (System.currentTimeMillis() > pooledObject.getCreateTime() + connectionTimeToLive) {
+                return false;
+            }
+        }
+        Socket socket = pooledObject.getObject();
+        return socket != null && !socket.isClosed();
+    }
+
+    @Override
+    public void activateObject(PooledObject<Socket> pooledObject) {
+        // do nothing
+    }
+
+    @Override
+    public void passivateObject(PooledObject<Socket> pooledObject) {
+        // do nothing
+    }
+}
---
+++ collectd/src/test/java/timely/collectd/plugin/WriteTimelyPluginTest.java
@@ -0,0 +1,203 @@
+package timely.collectd.plugin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+
+import org.collectd.api.Collectd;
+import org.collectd.api.DataSet;
+import org.collectd.api.DataSource;
+import org.collectd.api.OConfigItem;
+import org.collectd.api.ValueList;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Collectd.class})
+@PowerMockIgnore("javax.management.*")
+public class WriteTimelyPluginTest {
+
+    private WriteTimelyPlugin plugin = null;
+    private FakeServer server = null;
+
+    @Before
+    public void setup() throws Exception {
+        PowerMock.suppress(PowerMock.everythingDeclaredIn(Collectd.class));
+        server = new FakeServer();
+    }
+
+    private ValueList createMetric() {
+        ValueList val = new ValueList();
+        val.setHost("r001n01.localdomain");
+        val.setTime(System.currentTimeMillis());
+        val.setPlugin("hddtemp");
+        val.setPluginInstance(null);
+        val.setType("temperature");
+        val.setTypeInstance("sda");
+        val.setValues(Collections.singletonList((Number) new Double(2.0)));
+        DataSet ds = new DataSet("test");
+        ds.addDataSource(new DataSource("test", DataSource.TYPE_COUNTER, 0, 100));
+        val.setDataSet(ds);
+        return val;
+    }
+
+    private void setupPlugin() throws Exception {
+        OConfigItem host = new OConfigItem("Host");
+        host.addValue(server.getHost());
+        OConfigItem port = new OConfigItem("Port");
+        port.addValue(Integer.toString(server.getPort()));
+        OConfigItem config = new OConfigItem("");
+        config.addChild(host);
+        config.addChild(port);
+        plugin = new WriteTimelyPlugin();
+        Assert.assertEquals(0, plugin.config(config));
+        // Seed with one metric to create the connection
+        Assert.assertEquals(0, plugin.write(createMetric()));
+        Thread.sleep(100);
+        Assert.assertTrue(server.messageReceived());
+    }
+
+    @Test
+    public void testWrite() throws Exception {
+        Thread t = new Thread(server);
+        t.start();
+        setupPlugin();
+        while (!server.ready()) {
+            Thread.sleep(1000);
+        }
+        Assert.assertEquals(0, plugin.write(createMetric()));
+        Thread.sleep(100);
+        Assert.assertTrue(server.messageReceived());
+        plugin.shutdown();
+        server.shutdown();
+        t.join();
+    }
+
+    @Test
+    public void testWriteAfterServerRestart() throws Exception {
+        Thread t = new Thread(server);
+        t.start();
+        setupPlugin();
+        while (!server.ready()) {
+            Thread.sleep(1000);
+        }
+        Assert.assertEquals(0, plugin.write(createMetric()));
+        Thread.sleep(100);
+        Assert.assertTrue(server.messageReceived());
+        server.shutdown();
+        t.join();
+        Thread.sleep(2000);
+
+        server.create();
+        Thread t2 = new Thread(server);
+        t2.start();
+        // Need to call this again because the server is not guaranteed to be
+        // listening on the same local port as the first time that it was
+        // started
+        setupPlugin();
+        while (!server.ready()) {
+            Thread.sleep(1000);
+            // Keep sending metrics to plugin to force reconnect
+            int result = plugin.write(createMetric());
+            System.out.println("Wrote to client, result: " + result);
+            Assert.assertEquals(0, result);
+        }
+        Assert.assertEquals(0, plugin.write(createMetric()));
+        Thread.sleep(1000);
+        Assert.assertTrue(server.messageReceived());
+        plugin.shutdown();
+        server.shutdown();
+        t2.join();
+    }
+
+    private static class FakeServer implements Runnable {
+
+        private ServerSocket server = null;
+        private BufferedReader reader = null;
+        private String host = null;
+        private int port = 0;
+        private Socket s = null;
+        private boolean received = false;
+
+        public FakeServer() throws Exception {
+            create();
+        }
+
+        public void create() throws Exception {
+            server = new ServerSocket(port);
+            server.setReceiveBufferSize(8192);
+            host = server.getInetAddress().getHostAddress();
+            port = server.getLocalPort();
+            System.out.println("Local port " + port);
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public void shutdown() throws Exception {
+            System.out.println("Closing server.");
+            s.close();
+            s = null;
+            reader.close();
+            reader = null;
+            server.close();
+            server = null;
+        }
+
+        public boolean ready() throws Exception {
+            return (s != null && s.isConnected());
+        }
+
+        synchronized public boolean messageReceived() {
+            try {
+                return Boolean.valueOf(this.received);
+            } finally {
+                this.received = false;
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                s = server.accept();
+                System.out.println("Client connected.");
+                reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                while (server != null && !server.isClosed() && s != null && s.isConnected()) {
+                    String line;
+                    try {
+                        line = reader.readLine();
+                    } catch (Exception e) {
+                        // Might have been closed
+                        System.out.println("Closed by Exception: " + e.getMessage());
+                        return;
+                    }
+                    if (null != line) {
+                        synchronized (this) {
+                            this.received = true;
+                        }
+                        System.out.println(line);
+                    }
+                }
+            } catch (IOException e) {
+                Assert.fail(e.getMessage());
+            }
+            System.out.println("Exiting run method.");
+        }
+
+    }
+
+}
