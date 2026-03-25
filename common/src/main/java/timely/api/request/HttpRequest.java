c27a3d2bb6d06bf0986c6bc999b99df24503d762
Bill Oley <bill.oley@gmail.com>
2026-03-25 12:59:45 -0400

    HttpRequest implement ReferenceCounted and pass through to FullHttpRequest to avoid memory leaks

--- common/src/main/java/timely/api/request/HttpRequest.java
+++ common/src/main/java/timely/api/request/HttpRequest.java
@@ -1,10 +1,63 @@
 package timely.api.request;
 
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.util.ReferenceCounted;
 
-public interface HttpRequest extends Request {
+public interface HttpRequest extends Request, ReferenceCounted {
 
     public void setHttpRequest(FullHttpRequest httpRequest);
 
     public FullHttpRequest getHttpRequest();
+
+    default int refCnt() {
+        if (getHttpRequest() != null) {
+            return getHttpRequest().refCnt();
+        } else {
+            return 1;
+        }
+    }
+
+    default ReferenceCounted retain() {
+        if (getHttpRequest() != null) {
+            getHttpRequest().retain();
+        }
+        return this;
+    }
+
+    default ReferenceCounted retain(int increment) {
+        if (getHttpRequest() != null) {
+            getHttpRequest().retain(increment);
+        }
+        return this;
+    }
+
+    default ReferenceCounted touch() {
+        if (getHttpRequest() != null) {
+            getHttpRequest().touch();
+        }
+        return this;
+    }
+
+    default ReferenceCounted touch(Object hint) {
+        if (getHttpRequest() != null) {
+            getHttpRequest().touch(hint);
+        }
+        return this;
+    }
+
+    default boolean release() {
+        if (getHttpRequest() != null) {
+            return getHttpRequest().release();
+        } else {
+            return false;
+        }
+    }
+
+    default boolean release(int decrement) {
+        if (getHttpRequest() != null) {
+            return getHttpRequest().release(decrement);
+        } else {
+            return false;
+        }
+    }
 }
